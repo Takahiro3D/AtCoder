70412712a1ba6c0d207fdb13566b9d1863d19c5d
Takahiro3D <takahiro-kawai@outlook.com>
2024-06-16 14:33:15 +0000

    (#4) 使用ツールの変更

--- script/check.bash
+++ script/check.bash
@@ -3,11 +3,5 @@
 PROGRAM_PATH=$1
 PROBLEM_PATH=$(dirname $PROGRAM_PATH)
 
-if [[ $PROGRAM_PATH == *.py ]]; then
-  # Python file
-  oj test -S -N -c "python3 ${PROGRAM_PATH}" -d ${PROBLEM_PATH}/test/
-else
-  # Compiled file
-  oj test -S -N -c "${PROGRAM_PATH}" -d ${PROBLEM_PATH}/test/
-fi
-
+cd $PROBLEM_PATH
+atcoder-tools test
