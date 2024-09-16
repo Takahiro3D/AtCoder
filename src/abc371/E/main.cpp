6fd697905707b32e06b9fdfd43b2d38464a48d0e
Takahiro3D <takahiro-kawai@outlook.com>
2024-09-16 06:44:15 +0000

    abc371: review

--- src/abc371/E/main.cpp
+++ src/abc371/E/main.cpp
@@ -28,24 +28,35 @@ int main() {
   vector<int> A(N);
   for (auto& a : A) {
     cin >> a;
+    a--;
   }
 
-  vector<int> K(N);
-  set<int> S;
+  // 値xが存在するAのindex
+  vector<vector<int>> is(N);
   for (int i = 0; i < N; i++) {
-    S.emplace(A[i]);
-    K[i] = S.size();
+    is[A[i]].push_back(i);
   }
 
-  vector<int> K_sum(N + 1);
-  K_sum[1] = K[0];
-  for (int i = 0; i < N; i++) {
-    K_sum[i + 1] = K[i] + K_sum[i];
-  }
-
-  int ans = 0;
-  for (int i = 0; i < N; i++) {
-    ans += K[i + 1] - K[i];
+  auto sections = [](int64_t n) {
+    // nC2
+    return n * (n - 1) / 2;
+  };
+  int64_t ans = 0;
+  for (int x = 0; x < N; x++) {
+    // xを含まない区間数の累計
+    int64_t section_x = 0;
+    // 最後尾処理用のindexを挿入
+    is[x].push_back(N);
+    // 先頭区間index用初期値
+    int pre = -1;
+    // xの区間
+    for (auto i : is[x]) {
+      // xを含まない区間数を計算
+      section_x += sections(i - pre);
+      pre = i;
+    }
+    // xを含む区間数 = 全体の区間数 - xを含まない区間数
+    ans += sections(N + 1) - section_x;
   }
 
   cout << ans << endl;
