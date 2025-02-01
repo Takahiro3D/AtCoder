6b706d7be4cecc4ad718bbfeae3093e3279579af
Takahiro3D <takahiro-kawai@outlook.com>
2025-02-01 11:56:27 +0000

    Revert "Merge pull request #5 from Takahiro3D/4-update_prepare"
    This reverts commit e6b7da74b262d1c3f5ec4e699523ae072e919cf4, reversing
    changes made to 7742a34501da37e08e8f65b1f6438b66dc54d76a.

--- script/check.bash
+++ script/check.bash
@@ -3,5 +3,11 @@
 PROGRAM_PATH=$1
 PROBLEM_PATH=$(dirname $PROGRAM_PATH)
 
-cd $PROBLEM_PATH
-atcoder-tools test
+if [[ $PROGRAM_PATH == *.py ]]; then
+  # Python file
+  oj test -S -N -c "python3 ${PROGRAM_PATH}" -d ${PROBLEM_PATH}/test/
+else
+  # Compiled file
+  oj test -S -N -c "${PROGRAM_PATH}" -d ${PROBLEM_PATH}/test/
+fi
+
